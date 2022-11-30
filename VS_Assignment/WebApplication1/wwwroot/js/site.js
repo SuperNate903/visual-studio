8b39befca02ee0e1a50434e31617a99dcaf716f8
SuperNate903 <minecraft.nate04@outlook.com>
2022-11-30 14:54:44 -0800

    Added Assignment File

--- VS_Assignment/WebApplication1/wwwroot/js/site.js
+++ VS_Assignment/WebApplication1/wwwroot/js/site.js
@@ -2,3 +2,22 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+var id = null;
+function changeColor() {
+    var elem = document.getElementById("color");
+    var pos = 0;
+    clearInterval(id);
+    id = setInterval(frame, 10);
+    function frame() {
+        if (pos == 100) {
+            clearInterval(id);
+        } else {
+            pos++;
+            var color = 0xFF - (pos * 2.55) << 16 | 0xFF - (pos * 2.55) << 8 | 0xFF - (pos * 1.9);
+            elem.style.backgroundColor = `#${color.toString(16).padStart(6, "0")}`;
+        }
+    }
+}
+
+changeColor()
