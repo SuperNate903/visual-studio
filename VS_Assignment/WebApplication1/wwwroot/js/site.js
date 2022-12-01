b815c2fcea19b3a4ae1774a49d260dca22b7f49f
SuperNate903 <minecraft.nate04@outlook.com>
2022-11-30 18:30:29 -0800

    Changed the starter template

--- VS_Assignment/WebApplication1/wwwroot/js/site.js
+++ VS_Assignment/WebApplication1/wwwroot/js/site.js
@@ -10,11 +10,11 @@ function changeColor() {
     clearInterval(id);
     id = setInterval(frame, 10);
     function frame() {
-        if (pos == 100) {
+        if (pos == 400) {
             clearInterval(id);
         } else {
             pos++;
-            var color = 0xFF - (pos * 2.55) << 16 | 0xFF - (pos * 2.55) << 8 | 0xFF - (pos * 1.9);
+            var color = 0xFF - (pos * 0.6375) << 16 | 0xFF - (pos * 0.6375) << 8 | 0xFF - (pos * 0.475);
             elem.style.backgroundColor = `#${color.toString(16).padStart(6, "0")}`;
         }
     }
